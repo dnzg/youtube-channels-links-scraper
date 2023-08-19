f61ca473b210676f7e0879fb810e3bc981321700
Daniel Zagidullin <daniel.zagidullin@superside.com>
2023-08-19 18:04:42 +0200

    refactor: youtube api

--- pages/api/youtube.ts
+++ pages/api/youtube.ts
@@ -1,69 +1,79 @@
 import axios from "axios";
 import { stringify } from "csv-stringify/sync";
 
-export default function handler(request: any, response: any) {
+const URLs = [
+    "vk.com",
+    "vk.ru",
+    "tiktok.com",
+    "pinterest.com",
+    "facebook.com",
+    "twitter.com",
+    "instagram.com",
+    "t.me",
+];
+
+export default async function handler(request: any, response: any) {
     if (request.method !== "GET") {
-        response.status(400).json({ status: "wrong method" });
+        return response.status(400).json({ status: "wrong method" });
     }
 
     const { channelsFinal, isIdFinal } = request.query;
     const isId = isIdFinal === "id";
     const channels = JSON.parse(channelsFinal);
 
-    const link = isId
-        ? "https://yt.lemnoslife.com/channels?part=about&id="
-        : "https://yt.lemnoslife.com/channels?part=about&forUsername=";
+    const baseLink = "https://yt.lemnoslife.com/channels?part=about&";
+    const link = isId ? `${baseLink}id=` : `${baseLink}forUsername=`;
 
-    const obj = channels.map(async (username: string) => {
-        return await axios.get(link + username).then(async (res) => {
-            let linksAbout: any = {};
-            if (res.data && res.data.items && res.data.items[0]) {
-                res.data.items[0].about.links.forEach((link: any) => {
-                    if (link?.url?.includes("vk.com")) {
-                        linksAbout.VK = link.url;
-                    } else if (link?.url?.includes("tiktok.com")) {
-                        linksAbout.tiktok = link.url;
-                    } else if (link?.url?.includes("pinterest.com")) {
-                        linksAbout.pinterest = link.url;
-                    } else if (link?.url?.includes("facebook.com")) {
-                        linksAbout.facebook = link.url;
-                    } else if (link?.url?.includes("twitter.com")) {
-                        linksAbout.twitter = link.url;
-                    } else if (link?.url?.includes("instagram.com")) {
-                        linksAbout.instagram = link.url;
-                    } else if (link?.url?.includes("instagram.com")) {
-                        linksAbout.instagram = link.url;
-                    } else if (link?.url?.includes("t.me")) {
-                        linksAbout.telegram = link.url;
+    try {
+        const results = await Promise.all(
+            channels.map(async (username: string) => {
+                const un = username.replace("\n", "").replace(" ", "");
+
+                if (!un) {
+                    return null;
+                }
+
+                const res = await axios.get(link + un);
+
+                let linksAbout: any = {};
+
+                if (!res.data || !res.data.items || !res.data.items[0]) {
+                    return null;
+                }
+
+                res?.data?.items?.[0]?.about?.links?.forEach((link: any) => {
+                    const foundURL = URLs.find((url) =>
+                        link?.url?.includes(url)
+                    );
+
+                    if (foundURL) {
+                        const key = foundURL.split(".")[0];
+                        linksAbout[key] = link.url;
                     } else {
                         linksAbout.other = linksAbout.other
                             ? linksAbout.other + ", " + link.url
                             : link.url;
                     }
                 });
-                const object = {
+
+                return {
                     username,
-                    viewCount: res.data.items[0].about.stats.viewCount,
-                    location: res.data.items[0].about.details.location,
+                    viewCount: res.data.items[0].about.stats.viewCount || 0,
+                    location: res.data.items[0].about.details.location || "",
                     ...linksAbout,
                 };
-                return object;
-            }
-            return { username };
-        });
-    });
+            })
+        );
 
-    Promise.all(obj).then((data) => {
         let keysAvailable: any = {};
-        data.forEach((link) => {
+        results.forEach((link) => {
             Object.keys(link).forEach((key) => (keysAvailable[key] = true));
         });
 
-        const finaldata = data.map((link) => {
+        const finaldata = results.map((link) => {
             Object.keys(keysAvailable).forEach((key) => {
                 link[key] = link[key] || "";
             });
-
             return link;
         });
 
@@ -76,6 +86,11 @@ export default function handler(request: any, response: any) {
             "attachment;filename=channels.csv"
         );
         response.setHeader("Content-Type", "text/csv");
-        response.status(200).send(output);
-    });
+        return response.status(200).send(output);
+    } catch (error) {
+        console.error("Error processing channels:", JSON.stringify(error));
+        return response
+            .status(500)
+            .json({ error: "Failed to process channels" });
+    }
 }
